97c6543e033abefc475efa13959988292cd4f85e
Alyssa Clarke <alyssa.clarke00@gmail.com>
2019-12-30 16:58:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,5 +19,11 @@ def map_to_no_change(source_array)
 end 
 
 def map_to_double(source_array)
-  
+    new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * 2 ) # <== Unique work
+    i += 1
+  end
+  return new
 end
